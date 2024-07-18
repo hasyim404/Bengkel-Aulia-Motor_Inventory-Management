e81696abfb4dc5d86546a6f3078ee144e490f26f
hasyim404 <muhaisim7@gmail.com>
2024-07-18 16:06:40 +0700

    fix .env

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 process.env.OPENSSL_CONF;
+const METHOD = process.env.METHOD;
+
 const express = require("express");
 const cors = require("cors");
-const { createServer } = require("https");
+const { createServer } = require(METHOD);
 const routes = require("./routes");
 const path = require("path");
 const FileUpload = require("express-fileupload");
@@ -10,16 +12,16 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT;
-const METHOD = process.env.METHOD;
 const DOMAIN = process.env.DOMAIN;
 
+// If Running Local, disable options
 const options = {
   key: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/privkey.pem`),
   cert: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/fullchain.pem`),
 };
 
 const app = express();
-const server = createServer(options, app);
+const server = createServer(options, app); // If Running Local, disable options
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
@@ -42,11 +44,11 @@ app.use("/", (req, res) => {
 server.listen(PORT, () => {
   if (process.env.HOSTING === "true") {
     console.log(
-      `# Website running (Hosting) #\n- PORT : ${PORT}\n- URL: ${METHOD}${DOMAIN}`
+      `# Website running (Hosting) #\n- PORT : ${PORT}\n- URL: ${METHOD}://${DOMAIN}`
     );
   } else {
     console.log(
-      `# Website running (Local) #\n- PORT : ${PORT}\n- URL: ${METHOD}${DOMAIN}:${PORT}`
+      `# Website running (Local) #\n- PORT : ${PORT}\n- URL: ${METHOD}://${DOMAIN}:${PORT}`
     );
   }
 });
